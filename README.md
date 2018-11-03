6dd278e1260c562d5a5ddc3e5b9f61f4b2ba0deb
Stephon Fonrose <stephon@Stephons-iMac.local>
2018-11-03 16:50:02 -0700

    Uploading my Tip Calculator: Tipster

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my Tip Calculator Prework for CodePath
--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSelect: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calcTip(_ sender: Any) {
+        let tipPercents = [0.15, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercents[tipSelect.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 }
 
